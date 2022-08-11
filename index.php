6abecaba4aca9ae6a41847737ecdf632142f955c
Fred <fred@xpple.dev>
2022-08-11 04:04:22 +0200

    Add HTML template

--- index.php
+++ index.php
@@ -22,32 +22,65 @@ if ($hasArtistId = array_key_exists("artistId", $_GET)) {
     $result = $statement->fetchAll()[0];
     $artistName = $result["artist_name"];
     $artistIcon = $result["artist_icon"];
-    echo $artistName;
-    echo "<img src='data:image/jpeg;base64,".base64_encode($artistIcon)."' alt='artist_icon'/>";
 }
 if ($hasSongId = array_key_exists("songId", $_GET)) {
     $songId = $_GET["songId"];
-    $statement = $conn->prepare("SELECT song_title, artist_name, song_cover_image, song_lyrics FROM songs JOIN artists on songs.artist_id = artists.artist_id WHERE HEX(song_id) = :song_id");
+    $statement = $conn->prepare("SELECT song_title, songs.artist_id, artist_name, song_cover_image, song_lyrics FROM songs JOIN artists on songs.artist_id = artists.artist_id WHERE HEX(song_id) = :song_id");
     $statement->execute(array("song_id" => $songId));
     $statement->setFetchMode(PDO::FETCH_ASSOC);
     $result = $statement->fetchAll()[0];
     $songTitle = $result["song_title"];
+    $artistId = $result["artist_id"];
     $artistName = $result["artist_name"];
     $songCoverImage = $result["song_cover_image"];
+    $songCoverImageBase64 = base64_encode($songCoverImage);
     $songLyrics = $result["song_lyrics"];
-    echo $songTitle;
-    echo $artistName;
-    echo "<img src='data:image/jpeg;base64,".base64_encode($songCoverImage)."' alt='song_cover_image'/>";
-    echo $songLyrics;
 }
-?>
+if ($hasSongId) {
+    echo(<<<HTML
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>Rap Lyrics Top</title>
+    <title>$artistName - $songTitle | Rap Lyrics Top</title>
+
+    <link rel="stylesheet" href="./assets/style/main.css">
+    <link rel="stylesheet" href="./assets/style/lyrics.css">
+    <script src="./assets/script/module.js" type="module" async></script>
 </head>
 <body>
-Content Coming Soon. As a preview, check out <a href="/eminem/good-guy/">Good Guy by Eminem</a>.
+<header>
+    <nav>
+        <ol>
+            <li><a href="./">Home</a></li>
+            <li><a href="?artistId=$artistId">$artistName</a></li>
+            <li><a href="?songId=$songId">$songTitle</a></li>
+        </ol>
+    </nav>
+</header>
+<main>
+    <section id="song-info">
+        <div id="img-container">
+            <img src="data:image/jpeg;base64,$songCoverImageBase64" alt="$songTitle by $artistName"/>
+        </div>
+        <div id="about">
+            <h1>$songTitle</h1>
+            <h2>$artistName</h2>
+            <p>Song description</p>
+        </div>
+    </section>
+    <section id="lyrics">
+        <h1>Lyrics</h1>
+        $songLyrics
+    </section>
+    <section id="annotation">
+    </section>
+</main>
+<footer>
+    [Footer]
+</footer>
 </body>
 </html>
+
+HTML);
+}
