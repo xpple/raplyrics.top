0b979d96bfc0ab9a087d9279ef113d5daf31d16b
Fred <fred@xpple.dev>
2022-08-13 23:43:28 +0200

    Add initial command

--- php/connect.php
+++ php/connect.php
@@ -8,6 +8,7 @@ require_once("./login-data.php");
 try {
     $conn = new PDO("mysql:host=$server;port=$port;dbname=$dbname", $user, $pass);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $conn->setAttribute(PDO::MYSQL_ATTR_INIT_COMMAND, "SET NAMES utf8");
 } catch(PDOException $e) {
     die("Server error");
 }
