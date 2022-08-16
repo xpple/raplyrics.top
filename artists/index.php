ff6a21069ad20df41df62d2f87880a5110bef6ab
Fred <fred@xpple.dev>
2022-08-16 18:01:03 +0200

    Fix paths

--- artists/index.php
+++ artists/index.php
@@ -4,14 +4,14 @@ $requestURI = trim($requestURI, '/');
 $dirs = explode('/', $requestURI);
 $dirCount = count($dirs);
 if ($dirCount == 1) {
-    require_once("./php/artists.php");
+    require_once("../php/artists.php");
 } elseif ($dirCount == 2) {
     $artistDirectory = $dirs[1];
     if ($artistDirectory == "index.php") {
-        require_once("./php/artists.php");
+        require_once("../php/artists.php");
     } else {
-        require_once("./php/artist.php");
+        require_once("../php/artist.php");
     }
 } else {
-    require_once("./php/unknown.php");
+    require_once("../php/unknown.php");
 }
